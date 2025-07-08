9933649c7b6db08f0d17f28e254a3a5b5aa03269
CheyenneE21 <cae5299@psu.edu>
2025-07-07 22:49:07 -0400

    First day of CC -- website foundation set

---
+++ script.js
@@ -0,0 +1,5 @@
+console.log("Welcome to My Website!"); // Prints a message in the browser's console
+
+document.addEventListener("DOMContentLoaded", () => { // Runs the function after the page is fully loaded
+  // alert("Hello! Thanks for visiting."); // Shows a popup alert when the page loads
+});
